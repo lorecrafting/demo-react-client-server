72378e989781f1b20fc667437472a16863763948
Raymond <my@email.com>
2018-06-21 02:42:47 +0000

    asdf

--- server/server.js
+++ server/server.js
@@ -4,5 +4,7 @@ const cors = require('cors');
 
 app.use(cors());
 
+app.use(express.static(__dirname + '../build'))
+
 app.get('/api/hello', (req, res) => res.send('ELLLLO'))
 app.listen(8080, () => console.log('App listening on port 8080'))
