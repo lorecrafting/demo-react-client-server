9f9b9ffcf61b539f449f24f110308b7c33998487
Raymond Luong <raymondluong@Raymonds-MacBook-Pro.local>
2018-06-21 15:38:33 -1000

    express serve static files

--- server/server.js
+++ server/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, '../build')))
 
 app.use(cors());
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
 
 app.get('/api/hello', (req, res) => res.send('ELLLLO'))
 app.listen(8080, () => console.log('App listening on port 8080'))
